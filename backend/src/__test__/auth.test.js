e1c6df288fb1ce8f0bd3ca6412604be50dec1b22
freddy velarde <freddyvelarde59#gmail.com>
2022-07-05 12:09:16 -0400

    formatting code

--- backend/src/__test__/auth.test.js
+++ backend/src/__test__/auth.test.js
@@ -1,82 +1,78 @@
 import app from '../app.js';
-import request  from 'supertest';
+import request from 'supertest';
 // import sequelize from '../config/database/db.connection.js';
 
 export let user = {
-    username: 'admin_user', 
-    email: 'admin@gmail.com',
-    password: "admin123"
-}
+	username: 'admin_user',
+	email: 'admin@gmail.com',
+	password: 'admin123',
+};
 export let token = '';
 
-
-
 describe('POST Create user', () => {
-    const signUrl = '/api/auth/signup'; 
-    it('no email - missing email', async () => {
-        const response = await request(app)
-            .post(signUrl)
-            .send({
-                username: user.username,
-                email: '',
-                password: user.password
-            })
-        expect(response.body.message).toBe('No email. Please add your email to register your account')
-        expect(response.body.isCreated).toBe(false)
-        expect(response.body.status).toBe(500)
-    });
-    it('password incorrect - password length', async () => {
-        const response = await request(app)
-            .post(signUrl)
-            .send({
-                username: user.username,
-                email: user.email,
-                password: '1234' 
-            })
-        expect(response.body.message).toBe('Your password must be greater than 5 and less than 25 characteres')
-        expect(response.body.isCreated).toBe(false)
-    });
-    it('username incorrect - username length', async () => {
-        const response = await request(app)
-            .post(signUrl)
-            .send({
-                username: 'user',
-                email: user.email,
-                password: user.password
-            })
-        expect(response.body.message).toBe('Username must be greater than 5 and less than 30 characteres')
-        expect(response.body.isCreated).toBe(false)
-    });
-    it('user craeted successfully', async () => {
-        const response = await request(app)
-            .post(signUrl)
-            .send(user)
-        expect(response.body.message).toBe('Your account was created successfully')
-        expect(response.body.isCreated).toBe(true)
-    });
-    it('email already exists', async () => {
-        const response = await request(app)
-            .post(signUrl)
-            .send(user)
-        expect(response.body.message).toBe('The email is already used, please add a new email')
-        expect(response.body.isCreated).toBe(false)
-    });
+	const signUrl = '/api/auth/signup';
+	it('no email - missing email', async () => {
+		const response = await request(app).post(signUrl).send({
+			username: user.username,
+			email: '',
+			password: user.password,
+		});
+		expect(response.body.message).toBe(
+			'No email. Please add your email to register your account'
+		);
+		expect(response.body.isCreated).toBe(false);
+		expect(response.body.status).toBe(500);
+	});
+	it('password incorrect - password length', async () => {
+		const response = await request(app).post(signUrl).send({
+			username: user.username,
+			email: user.email,
+			password: '1234',
+		});
+		expect(response.body.message).toBe(
+			'Your password must be greater than 5 and less than 25 characteres'
+		);
+		expect(response.body.isCreated).toBe(false);
+	});
+	it('username incorrect - username length', async () => {
+		const response = await request(app).post(signUrl).send({
+			username: 'user',
+			email: user.email,
+			password: user.password,
+		});
+		expect(response.body.message).toBe(
+			'Username must be greater than 5 and less than 30 characteres'
+		);
+		expect(response.body.isCreated).toBe(false);
+	});
+	it('user craeted successfully', async () => {
+		const response = await request(app).post(signUrl).send(user);
+		expect(response.body.message).toBe(
+			'Your account was created successfully'
+		);
+		expect(response.body.isCreated).toBe(true);
+	});
+	it('email already exists', async () => {
+		const response = await request(app).post(signUrl).send(user);
+		expect(response.body.message).toBe(
+			'The email is already used, please add a new email'
+		);
+		expect(response.body.isCreated).toBe(false);
+	});
 });
 describe('POST Create user', () => {
-    const logInUrl = '/api/auth/login'; 
+	const logInUrl = '/api/auth/login';
 
-    it('logIn successfully', async () => {
-        const response = await request(app)
-            .post(logInUrl)
-            .send({
-                password: user.password,
-                email: user.email
-            })
-        expect(response.body.auth).toBe(true)
-        token = response.body.token        
-    });
-    // it('', async () => {});
-    // it('', async () => {});
-    // it('', async () => {});
-    // it('', async () => {});
+	it('logIn successfully', async () => {
+		const response = await request(app).post(logInUrl).send({
+			password: user.password,
+			email: user.email,
+		});
+		expect(response.body.auth).toBe(true);
+		token = response.body.token;
+	});
+	// it('', async () => {});
+	// it('', async () => {});
+	// it('', async () => {});
+	// it('', async () => {});
 });
--- backend/src/__test__/users.test.js
+++ backend/src/__test__/users.test.js
@@ -3,38 +3,34 @@ import request from 'supertest';
 import { token, user } from './auth.test.js';
 
 describe('GET /api/users/all', () => {
-    const url = '/api/users/all'
-    it('There are no users added', async () => {
-        const res = await request(app)
-            .get(url)
-            .send()
-        expect(res.statusCode).toBe(200)
-        expect(res.body.message).toEqual('there are no users yet')
-    });
-    // it('There are users', async () => {
-    //     const res = await request(app)
-    //         .get(url)
-    //         .send()
-    //     expect(res.body).toEqual([]);
-    // });
-    // it('', async () => {});
-    // it('', async () => {});
-    // it('', async () => {});
-    // it('', async () => {});
-    // it('', async () => {});
+	const url = '/api/users/all';
+	it('There are no users added', async () => {
+		const res = await request(app).get(url).send();
+		expect(res.statusCode).toBe(200);
+		expect(res.body.message).toEqual('there are no users yet');
+	});
+	// it('There are users', async () => {
+	//     const res = await request(app)
+	//         .get(url)
+	//         .send()
+	//     expect(res.body).toEqual([]);
+	// });
+	// it('', async () => {});
+	// it('', async () => {});
+	// it('', async () => {});
+	// it('', async () => {});
+	// it('', async () => {});
 });
 
 describe('DELETE remove the user', () => {
-    const deleteUrl = '/api/users/remove'
-    it('user deleted successfully', async () => {
-        const response = await request(app)
-            .delete(deleteUrl)
-            .set('access-token', token)
-            .send({password: user.password})
-        expect(response.body.isRemoved).toBe(true)
-    });
+	const deleteUrl = '/api/users/remove';
+	it('user deleted successfully', async () => {
+		const response = await request(app)
+			.delete(deleteUrl)
+			.set('access-token', token)
+			.send({ password: user.password });
+		expect(response.body.isRemoved).toBe(true);
+	});
 
-    // it('', async () => {});
+	// it('', async () => {});
 });
-
-
